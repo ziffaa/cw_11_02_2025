7c8f9b077517bd9715bade81a7cec1bff41e7fd4
ziffa <rforkaliuk@gmail.com>
2025-02-12 11:19:09 +0200

    Add project files.

---
+++ cw_11_02_2025/cw_11_02_2025.cpp
@@ -0,0 +1,94 @@
+#include <windows.h>
+#include "resource.h"
+#include <tchar.h>
+
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+
+TCHAR szClassWindow[] = TEXT("Каркасное приложение");
+HICON hIcon;
+HCURSOR hCursor1, hCursor2, hCursor3;
+
+int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Msg;
+	WNDCLASSEX wcl;
+	wcl.cbSize = sizeof(wcl);
+	wcl.style = CS_HREDRAW | CS_VREDRAW;
+	wcl.lpfnWndProc = WindowProc;
+	wcl.cbClsExtra = 0;
+	wcl.cbWndExtra = 0;
+	wcl.hInstance = hInst;
+	wcl.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1));
+	wcl .hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcl.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcl.lpszMenuName = NULL;
+	wcl.lpszClassName = szClassWindow;
+	wcl.hIconSm = NULL;
+	if (!RegisterClassEx(&wcl))
+		return 0;
+	hWnd = CreateWindowEx(0, szClassWindow, TEXT("Ресурсы"), WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInst, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&Msg, NULL, 0, 0))
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CREATE:
+	{
+		HINSTANCE hInstance = GetModuleHandle(0);
+		hCursor1 = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR2));
+		hCursor2 = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR4));
+		hCursor3 = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR5));
+	}
+	break;
+	case WM_MOUSEMOVE:
+	{
+		// устанавливаем тот или иной курсор в зависимости от местонахождения указателя мыши
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+		if (x <= rect.right / 3)
+			SetCursor(hCursor1);
+		else if (x >= rect.right * 2 / 3)
+			SetCursor(hCursor2);
+		else
+			SetCursor(hCursor3);
+	}
+	break;
+	case WM_LBUTTONDOWN:
+	{
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+
+		if (x <= rect.right / 3)
+			MessageBox(0, _T("LEFT"), _T(""), 0);
+		else if (x >= rect.right * 2 / 3)
+			MessageBox(0, _T("RIGHT"), _T(""), 0);
+		else
+			MessageBox(0, _T("MIDDLE"), _T(""), 0);
+	}
+	break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
---
+++ cw_11_02_2025/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by cw_11_02_2025.rc
+//
+#define IDC_MYICON                      2
+#define IDD_CW11022025_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDC_POINTER                     104
+#define IDM_EXIT                        105
+#define IDI_CW11022025                  107
+#define IDI_SMALL                       108
+#define IDC_CW11022025                  109
+#define IDR_MAINFRAME                   128
+#define IDC_CURSOR1                     129
+#define IDC_CURSOR2                     130
+#define IDC_CURSOR3                     131
+#define IDC_CURSOR4                     131
+#define IDC_CURSOR5                     132
+#define IDI_ICON1                       133
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
